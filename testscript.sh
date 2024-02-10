83c1a370715678fcafb1ede4b826df9119abd649
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-02-11 00:06:44 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,5 @@ sudo apt update -y
 sudo apt install apache2 -y
 sudo service apache2 start
 cd /var/www/html
-sudo echo "<html><h1>Hello, World! This webpage belongs to Raghav....</h1></html>" > homepage.html
+sudo touch homepage.html
+sudo echo "<html><h1>Hello, World! This webpage belongs to Raghav....</h1><a href="https://youtu.be/zqGW6x_5N0k">ANIMAL: ARJAN VAILLY | Ranbir Kapoor | Sandeep Vanga | Bhupinder B, Manan B | Bhushan K</a></html>" > homepage.html
