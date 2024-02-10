0052adbca58db4f763705a10a8516708c37fbbe1
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-02-10 23:45:30 +0530

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -4,4 +4,4 @@ sudo apt update -y
 sudo apt install apache2 -y
 sudo service apache2 start
 cd /var/www/html
-sudo echo "<html><h1>Hello, World! This webpage belongs to Raghav....</h1></html>" >> index.html
+sudo echo "<html><h1>Hello, World! This webpage belongs to Raghav....</h1></html>" > index.html
