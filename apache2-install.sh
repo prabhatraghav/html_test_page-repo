2e14bdd16d68a6307bba5f855ac2eecbebf21065
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-02-17 00:38:15 +0530

    Update apache2-install.sh

--- apache2-install.sh
+++ apache2-install.sh
@@ -10,6 +10,8 @@ sudo wget https://raw.githubusercontent.com/prabhatraghav/html_test_page-repo/ma
 sudo wget https://raw.githubusercontent.com/prabhatraghav/html_test_page-repo/main/mediaplayer/mediaplayer.html
 sudo wget https://raw.githubusercontent.com/prabhatraghav/html_test_page-repo/main/mediaplayer/media-player.css
 sudo wget https://raw.githubusercontent.com/prabhatraghav/html_test_page-repo/main/mediaplayer/media-player.js
+sudo touch homepage.html
+sudo echo "<html><h1>Hello, World! This webpage belongs to Raghav....</h1><a href="https://youtu.be/zqGW6x_5N0k">ANIMAL: ARJAN VAILLY | Ranbir Kapoor | Sandeep Vanga | Bhupinder B, Manan B | Bhushan K</a></html>" > homepage.html
 sudo mkdir files
 sudo chmod 755 files
 cd /var/www/html/files
@@ -17,5 +19,3 @@ sudo wget https://raw.githubusercontent.com/prabhatraghav/html_test_page-repo/ma
 sudo wget https://raw.githubusercontent.com/prabhatraghav/html_test_page-repo/main/media/bazaar.mp4
 sudo chmod 755 *
 sudo service apache2 restart
-sudo touch homepage.html
-sudo echo "<html><h1>Hello, World! This webpage belongs to Raghav....</h1><a href="https://youtu.be/zqGW6x_5N0k">ANIMAL: ARJAN VAILLY | Ranbir Kapoor | Sandeep Vanga | Bhupinder B, Manan B | Bhushan K</a></html>" > homepage.html
